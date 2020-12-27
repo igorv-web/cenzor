a49ef62ddc3c2cb06b21bf40b609bc73b1e5ae63
igorv-web <igorvilkov404@gmail.com>
2020-12-27 19:55:49 +0200

    cenzor on Angular v1.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CenzorComponent } from './cenzor/cenzor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CenzorComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
